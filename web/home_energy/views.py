473c2a2b42f2224a6665a9606ff4acc5ac44eab1
pronsman <peter.ronsmans@nokia.com>
2022-02-16 21:24:42 +0100

    UI: Added basic authentication
    -login page /auth/login
    -logout page /auth/logout
    -login is required for all views in the home-energy app

--- web/home_energy/views.py
+++ web/home_energy/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.http import Http404
+from django.shortcuts import render
 from home_energy.view import get_latest
 from home_energy.view import get_detailed_usage
 from home_energy.view import p1_reader_debug
 
-#@login_required(login_url='/accounts/login/')
-
+@login_required
 def usage(request):
     return render(request, 'home_energy/usage.html')
 
+@login_required
 def latest(request):
     return HttpResponse(get_latest.get_latest(), content_type="application/json")
 
+@login_required
 def details(request):
     date = request.GET.get('date', '')
     mode = request.GET.get('mode', 1)
     return HttpResponse(get_detailed_usage.get_detailed_usage(date, mode), content_type="application/json")
 
+@login_required
 def debug_p1_reader(request):
     context = {'debug_entries': p1_reader_debug.get_debug_entries()}
     return render(request, 'home_energy/debug/p1-reader.html', context)
--- web/home_portal/urls.py
+++ web/home_portal/urls.py
@@ -1,7 +1,7 @@
 """home_portal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('home-energy/', include('home_energy.urls')),
     path('admin/', admin.site.urls),
-    #path('accounts/', include('django.contrib.auth.urls')), # login
+    path('auth/', include('django.contrib.auth.urls')),
 ]
