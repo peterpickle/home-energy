88f6593c933fe86b23bbcbd1d98fa71c72194977
pronsman <peter.ronsmans@nokia.com>
2022-03-03 17:45:50 +0100

    Django: avoid unnecessary redirect to latest/" when sending a request to "latest"

--- web/home_energy/urls.py
+++ web/home_energy/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('', views.usage, name='usage'),
     # ex: /home-energy/usage.html
     path('usage.html', views.usage, name='usage'),
-    # ex: /home-energy/latest/
-    path('latest/', views.latest, name='latest'),
-    # ex: /home-energy/details/
-    path('details/', views.details, name='details'),
+    # ex: /home-energy/latest
+    path('latest', views.latest, name='latest'),
+    # ex: /home-energy/details
+    path('details', views.details, name='details'),
 
     # ex: /home-energy/debug/p1-reader.html
     path('debug/p1-reader.html', views.debug_p1_reader, name='debug_p1_reader'),
